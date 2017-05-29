c4237e621a62bf8a699c7a0eb975aa8a53c5104e
vladimir.ventaniuc <vladimir.ventaniuc@yahoo.com>
2017-05-29 10:07:55 +0300

    danger_zone

--- kimo_py/live_map/static/js/danger.js
+++ kimo_py/live_map/static/js/danger.js
@@ -32,6 +32,7 @@ class Polygons {
 
 
 function dangerMap() {
+
 var mapProp= {
     center:new google.maps.LatLng(47.158374,27.601104),
     zoom:13,
@@ -46,6 +47,9 @@ addShape(map);
 
 function addShape(map){
 
+var elem = document.getElementById("divform");
+//elem.style.display = "none";
+//elem.style.visibility = "hidden";
 var drawingManager = new google.maps.drawing.DrawingManager({
 
           drawingControl: true,
@@ -61,18 +65,42 @@ var drawingManager = new google.maps.drawing.DrawingManager({
           }
         });
        google.maps.event.addListener(drawingManager, 'overlaycomplete', function(polygon) {
-    var coordinatesArray = polygon.overlay.getPath().getArray();
-    polygon = new Polygon();
-    polygon.add_points(coordinatesArray);
-    for(i=0; i<coordinatesArray.length;i++)
-    {
-       console.log( coordinatesArray[i]+ '   ');
-    }
-    console.log(',')
+            var coordinatesArray = polygon.overlay.getPath().getArray();
+            polygon = new Polygon();
+            polygon.add_points(coordinatesArray);
+            for(i=0; i<coordinatesArray.length;i++)
+             {
+                 console.log( coordinatesArray[i]+ '   ');
+             }
+            console.log(',')
+            elem.style.visibility = "visible";
+
+            document.getElementById("submitbt").addEventListener("click", function(){
+                 var name = document.getElementById("name").value;
+                 var description = document.getElementById("description").value;
+
+                 console.log(name);
+                 console.log(description);
+
+              //  document.name.value='';
+                // description.value='';
+
+                var obj= new Object();
+                obj.name=name;
+                obj.description=description;
+                obj.coordinates=polygon.get_list();
+
+                var jsonString= JSON.stringify(obj);
+
+                console.log(jsonString);
+
+                 elem.style.visibility = "hidden";
+                });
 
 
-
-
+            //var name = document.getElementById("name").value;
+           // var description = document.getElementById("description").value;
+           // console.log(name);
 
 
 
