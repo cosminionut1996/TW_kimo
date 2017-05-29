d79b494534fbec6134808e76ebbd9d544f245bd6
vladimir.ventaniuc <vladimir.ventaniuc@yahoo.com>
2017-05-29 16:02:37 +0300

    danger

--- kimo_py/live_map/static/js/danger.js
+++ kimo_py/live_map/static/js/danger.js
@@ -66,6 +66,7 @@ var drawingManager = new google.maps.drawing.DrawingManager({
         });
        google.maps.event.addListener(drawingManager, 'overlaycomplete', function(polygon) {
             var coordinatesArray = polygon.overlay.getPath().getArray();
+            var p=0;
             polygon = new Polygon();
             polygon.add_points(coordinatesArray);
             for(i=0; i<coordinatesArray.length;i++)
@@ -78,13 +79,13 @@ var drawingManager = new google.maps.drawing.DrawingManager({
             document.getElementById("submitbt").addEventListener("click", function(){
                  var name = document.getElementById("name").value;
                  var description = document.getElementById("description").value;
-
                  console.log(name);
                  console.log(description);
 
               //  document.name.value='';
                 // description.value='';
-
+                p=p+1
+                console.log(p);
                 var obj= new Object();
                 obj.name=name;
                 obj.description=description;
@@ -92,8 +93,10 @@ var drawingManager = new google.maps.drawing.DrawingManager({
 
                 var jsonString= JSON.stringify(obj);
 
+               // post('http://127.0.0.1:8000/live_map/', jsonString);
                 console.log(jsonString);
-
+                document.getElementById("json").value=jsonString;
+                console.log(document.getElementById("json").value);
                  elem.style.visibility = "hidden";
                 });
 
@@ -108,4 +111,31 @@ var drawingManager = new google.maps.drawing.DrawingManager({
 
         drawingManager.setMap(map);
 
+}
+
+
+
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
 }
