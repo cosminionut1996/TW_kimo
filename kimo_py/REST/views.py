c0b9948874a2264053ed8d69fde4b064c298faf0
cschifirnet <cosminionut1996@yahoo.com>
2017-06-07 23:16:34 +0300

    CheckChildren

--- kimo_py/REST/views.py
+++ kimo_py/REST/views.py
@@ -3,6 +3,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from kimo.models import Device
 
+from django.http import HttpResponseRedirect
+
+
+class RequestPictureView(APIView):
+    def post(self, request, format=None):
+        poza = request.data.get('poza')
+        if not poza:
+            return Response("Required fields not completed", status=status.HTTP_400_BAD_REQUEST)
+
+
+class CheckChildren(APIView):
+    def get(self, request, format=None):
+        
+        return Response("Done", status=status.HTTP_400_BAD_REQUEST)
+
 
 class LocationView(APIView):
     def put(self, request, token, format=None):
@@ -10,7 +25,6 @@ class LocationView(APIView):
         latitudine = request.data.get('latitudine')
         if not longitudine or not latitudine:
             return Response("Required fields not completed", status=status.HTTP_400_BAD_REQUEST)
-
         try:
             Device.objects.filter(token=token).update(
                 longitudine=longitudine,
@@ -18,5 +32,5 @@ class LocationView(APIView):
             )
         except:
             return Response("Token does not exist", status=status.HTTP_400_BAD_REQUEST)
-
         return Response("", status=status.HTTP_201_CREATED)
+
