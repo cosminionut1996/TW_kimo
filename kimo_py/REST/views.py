68c881a6faade3ca4be52fe684c9cbe52a82d47f
vladimir.ventaniuc <vladimir.ventaniuc@yahoo.com>
2017-06-08 00:33:40 +0300

    query

--- kimo_py/REST/views.py
+++ kimo_py/REST/views.py
@@ -5,6 +5,8 @@ from kimo.models import Device
 
 from django.http import HttpResponseRedirect
 
+from settings import SESSION_USER_ID_FIELD_NAME
+
 
 class RequestPictureView(APIView):
     def post(self, request, format=None):
@@ -15,7 +17,12 @@ class RequestPictureView(APIView):
 
 class CheckChildren(APIView):
     def get(self, request, format=None):
+        Device.objects.raw(
+            'select u.id, d.latitudine, d.longitudine, r.coordinates from utilizator u join legatura l on l.id_parinte=u.id join copil c on c.id=l.id_copil join device d on d.id_copil=c.id  join zona_risc r on u.id= r.id_utilizator where u.id={}'.format(
+                request.session.get(SESSION_USER_ID_FIELD_NAME)))
+
         return Response("Done", status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class LocationView(APIView):
