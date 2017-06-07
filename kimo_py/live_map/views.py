0f666f7ebb1db004c5a9ec516a48722f3fe40290
cschifirnet <cosminionut1996@yahoo.com>
2017-06-07 11:55:06 +0300

    fixes

--- kimo_py/live_map/views.py
+++ kimo_py/live_map/views.py
@@ -11,7 +11,9 @@ class Map(View):
     @logged_in_only
     def get(self, request):
         return render(request, 'live_map/google_map.html', context={
-            'zone': ZonaRisc.objects.filter()
+            'zone': ZonaRisc.objects.filter(
+                id_utilizator=request.session.get(SESSION_USER_ID_FIELD_NAME)
+            )
         })
 
 
@@ -26,10 +28,10 @@ class Danger(View):
         zone_name = d.get("name")
         zone_description = d.get("description")
         zone_coordinates = d.get("coordinates")
-        id_utilizator = request.sesion[SESSION_USER_ID_FIELD_NAME]
+        id_utilizator = request.session.get(SESSION_USER_ID_FIELD_NAME)
         new_zone = ZonaRisc(name=zone_name,
                             description=zone_description,
                             coordinates=zone_coordinates,
-                            id_utilizator= id_utilizator)
+                            id_utilizator=id_utilizator)
         new_zone.save()
         return render(request, 'live_map/danger_area.html')
